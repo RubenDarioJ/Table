761704beeb7359a5a216183e1e0a245ce5a5ab8a
RubenDarioJ <rubendjusto02@gmail.com>
2024-02-10 01:19:30 -0400

    improved button and added responsive images

--- anime.js
+++ anime.js
@@ -1,5 +1,3 @@
-// const { filter } = require("vue/types/umd");
-
 async function getAnimeData() {
     const url = "https://api.nekosapi.com/v3/images";
     let data = [];
@@ -15,6 +13,71 @@ async function getAnimeData() {
     return data;
 }
 
+async function renderImage() {
+    const animeData = await getAnimeData();
+
+    const tableElement = document.querySelector(".custom-table");
+
+    animeData.forEach((user) => {
+        const trElement = document.createElement("tr");
+
+        //mostrar ID
+        let pElementId = document.createElement("p");
+        let tdElement = document.createElement("td");
+        tdElement.classList.add("td-id");
+        pElementId.innerHTML = user.id;
+        tdElement.appendChild(pElementId);
+        tdElement.style.textAlign = "center";
+        trElement.appendChild(tdElement);
+
+        //rating de la imagen
+        const pElementRating = document.createElement("p");
+        const tdElementText = document.createElement("td");
+        tdElementText.classList.add("td-text");
+        pElementRating.innerHTML = user.rating;
+        tdElementText.appendChild(pElementRating);
+        trElement.appendChild(tdElementText);
+
+        //mostrar imagen
+        const imgElement = document.createElement("img");
+        let tdElementImg = document.createElement("td");
+        tdElementImg.classList.add("td-image");
+        imgElement.src = user.image_url;
+        tdElementImg.appendChild(imgElement);
+        trElement.appendChild(tdElementImg);
+
+        //esconder imagen click function
+        const nsfwButton = document.querySelector(".button-filter, .td-text");
+        nsfwButton.addEventListener("click", () => {
+            if(user.rating != 'safe') {
+            pElementId.style.display = "none";
+            tdElement.style.visibility ="hidden";
+            pElementRating.style.display = "none";
+            tdElementText.style.visibility ="hidden";
+            imgElement.style.display = "none";
+            tdElementImg.style.display ="none";
+        };
+});
+
+        //mostrar imagen click function
+        const nsfwDisplay = document.querySelector(".button-display");
+        nsfwDisplay.addEventListener("click", () => {
+            if(imgElement.style.display = "none") {
+            pElementId.style.display = "block";
+            tdElement.style.visibility ="visible";
+            pElementRating.style.display = "block";
+            tdElementText.style.visibility ="visible";
+            tdElementImg.style.display ="flex";
+            imgElement.style.display = "block";
+                };
+        });
+
+        tableElement.querySelector("tbody").appendChild(trElement);
+    })
+}
+
+renderImage();
+
 
 // async function filterNSFW() {
 //     const filter = await getAnimeData();
@@ -23,73 +86,8 @@ async function getAnimeData() {
 //         if(user.rating != 'safe') {
 //             // console.log('funciona');
 //             // hideElement.style.display = "none";
-//             animeImage.style.display= "none";
+//             imgElement.style.display= "none";
 //         }
 //     })
 //     return filter;
 // }
-
-// // filterNSFW();
-
-// function filterNSFW(animeImage, user) {
-//     if(user.rating != 'safe') {
-//         animeImage.style.display = "none";
-// }
-// }
-
-// function bringItBack(animeImage, user) {
-//     if(user.rating) {
-//         animeImage.style.display = "block";
-//     }
-// }
-
-async function renderImage() {
-    const animeData = await getAnimeData();
-    // const filterData = await filterNSFW();
-
-    const tableElement = document.querySelector(".custom-table");
-
-    animeData.forEach((user) => {
-        const trElement = document.createElement("tr");
-
-        //mostrar ID
-        let tdEelement = document.createElement("td");
-        tdEelement.innerHTML = user.id;
-        tdEelement.style.textAlign = "center";
-        trElement.appendChild(tdEelement);
-
-        //rating de la imagen
-
-        tdEelement = document.createElement("td");
-        tdEelement.classList.add("td-text");
-        tdEelement.innerHTML = user.rating;
-        trElement.appendChild(tdEelement);
-
-        //mostrar imagen
-        const animeImage = document.createElement("img");
-        tdEelement = document.createElement("td");
-        tdEelement.classList.add("td-image");
-        animeImage.src = user.image_url;
-        tdEelement.appendChild(animeImage);
-        trElement.appendChild(tdEelement);
-
-        //esconder imagen
-        // filterNSFW(animeImage, user);
-
-        const nsfwButton = document.querySelector(".button-filter");
-        nsfwButton.addEventListener("click", () => {
-            if(user.rating != 'safe') {
-            animeImage.style.display = "none";
-        };
-});
-        const nsfwDisplay = document.querySelector(".button-display");
-                nsfwDisplay.addEventListener("click", () => {
-                    if(animeImage.style.display = "none") {
-                    animeImage.style.display = "block";
-                };
-        });
-        tableElement.querySelector("tbody").appendChild(trElement);
-    })
-}
-
-renderImage();
